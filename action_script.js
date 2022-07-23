2535480cda9408625d1ecac1881e67afc46871c8
Dave Laffan <dave.laffan@outlook.com>
2022-07-23 16:26:45 +0100

    Add icons

--- action_script.js
+++ action_script.js
@@ -2,31 +2,38 @@
 
 const currentUrl = new URL(window.location.href);
 
-switch (true) {
-  case currentUrl.pathname.includes('/products'):
-    redirect(
-      currentUrl,
-      'products',
-      'var meta = {"product":{"id":',
-      ',"gid":"gid:'
-    );
-    break;
+if (currentUrl.host == 'e-liquids.com') {
+  switch (true) {
+    case currentUrl.pathname.includes('/products'):
+      redirect(
+        currentUrl,
+        'products',
+        'var meta = {"product":{"id":',
+        ',"gid":"gid:'
+      );
+      break;
 
-  case currentUrl.pathname.includes('/collections'):
-    redirect(
-      currentUrl,
-      'collections',
-      '"resourceType":"collection","resourceId":',
-      '}};'
-    );
-    break;
+    case currentUrl.pathname.includes('/collections'):
+      redirect(
+        currentUrl,
+        'collections',
+        '"resourceType":"collection","resourceId":',
+        '}};'
+      );
+      break;
 
-  case currentUrl.pathname.includes('/pages'):
-    redirect(currentUrl, 'pages', '"resourceType":"page","resourceId":', '}};');
-    break;
+    case currentUrl.pathname.includes('/pages'):
+      redirect(
+        currentUrl,
+        'pages',
+        '"resourceType":"page","resourceId":',
+        '}};'
+      );
+      break;
 
-  default:
-    window.open(`https://e-liquids-1.myshopify.com/admin`);
+    default:
+      window.open(`https://e-liquids-1.myshopify.com/admin`);
+  }
 }
 
 async function redirect(url, template, startSplit, endSplit) {
