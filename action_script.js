61fb3251b2d00fea8cab1efffee0270a21da51cb
Dave Laffan <dave.laffan@outlook.com>
2022-07-23 15:48:52 +0100

    Update to redirect to collection & page admins

--- action_script.js
+++ action_script.js
@@ -3,12 +3,30 @@
 const currentUrl = new URL(window.location.href)
 
 if (currentUrl.pathname.includes('/products')) {
-  redirectProductPage(currentUrl)
+  redirectProduct(currentUrl)
+} else if (currentUrl.pathname.includes('/collections')) {
+  redirectCollection(currentUrl)
+} else if (currentUrl.pathname.includes('/pages')) {
+  redirectPage(currentUrl)
 }
 
-async function redirectProductPage(url) {
+async function redirectProduct(url) {
   let response = await fetch(url)
   let rText = await response.text()
   let productId = rText.split('var meta = {"product":{"id":')[1].split(',"gid":"gid:')[0]
   window.open(`https://e-liquids-1.myshopify.com/admin/products/${productId}`);
+}
+
+async function redirectCollection(url) {
+  let response = await fetch(url)
+  let rText = await response.text()
+  let collectionId = rText.split('"resourceType":"collection","resourceId":')[1].split('}};')[0]
+  window.open(`https://e-liquids-1.myshopify.com/admin/collections/${collectionId}`);
+}
+
+async function redirectPage(url) {
+  let response = await fetch(url)
+  let rText = await response.text()
+  let pageId = rText.split('"resourceType":"page","resourceId":')[1].split('}};')[0]
+  window.open(`https://e-liquids-1.myshopify.com/admin/pages/${pageId}`);
 }
