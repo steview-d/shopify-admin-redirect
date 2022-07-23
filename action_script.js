30a140035b7e0193a5e48f91512625ce1bbf5c29
Dave Laffan <dave.laffan@outlook.com>
2022-07-23 16:09:34 +0100

    Refactor

--- action_script.js
+++ action_script.js
@@ -1,32 +1,37 @@
 // Run this when the extenstion icon is clicked
 
-const currentUrl = new URL(window.location.href)
+const currentUrl = new URL(window.location.href);
 
-if (currentUrl.pathname.includes('/products')) {
-  redirectProduct(currentUrl)
-} else if (currentUrl.pathname.includes('/collections')) {
-  redirectCollection(currentUrl)
-} else if (currentUrl.pathname.includes('/pages')) {
-  redirectPage(currentUrl)
+switch (true) {
+  case currentUrl.pathname.includes('/products'):
+    redirect(
+      currentUrl,
+      'products',
+      'var meta = {"product":{"id":',
+      ',"gid":"gid:'
+    );
+    break;
+
+  case currentUrl.pathname.includes('/collections'):
+    redirect(
+      currentUrl,
+      'collections',
+      '"resourceType":"collection","resourceId":',
+      '}};'
+    );
+    break;
+
+  case currentUrl.pathname.includes('/pages'):
+    redirect(currentUrl, 'pages', '"resourceType":"page","resourceId":', '}};');
+    break;
+
+  default:
+    window.open(`https://e-liquids-1.myshopify.com/admin`);
 }
 
-async function redirectProduct(url) {
-  let response = await fetch(url)
-  let rText = await response.text()
-  let productId = rText.split('var meta = {"product":{"id":')[1].split(',"gid":"gid:')[0]
-  window.open(`https://e-liquids-1.myshopify.com/admin/products/${productId}`);
-}
-
-async function redirectCollection(url) {
-  let response = await fetch(url)
-  let rText = await response.text()
-  let collectionId = rText.split('"resourceType":"collection","resourceId":')[1].split('}};')[0]
-  window.open(`https://e-liquids-1.myshopify.com/admin/collections/${collectionId}`);
-}
-
-async function redirectPage(url) {
-  let response = await fetch(url)
-  let rText = await response.text()
-  let pageId = rText.split('"resourceType":"page","resourceId":')[1].split('}};')[0]
-  window.open(`https://e-liquids-1.myshopify.com/admin/pages/${pageId}`);
+async function redirect(url, template, startSplit, endSplit) {
+  let response = await fetch(url);
+  let rText = await response.text();
+  let id = rText.split(startSplit)[1].split(endSplit)[0];
+  window.open(`https://e-liquids-1.myshopify.com/admin/${template}/${id}`);
 }
