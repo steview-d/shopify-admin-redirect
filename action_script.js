68f126268c748a1f4165cddd7cb1728eb08a9e59
Dave Laffan <dave.laffan@outlook.com>
2022-07-24 11:31:38 +0100

    Rewrite action_script
    Pull in .json data instead of parsing text and using dodgy splits.

--- action_script.js
+++ action_script.js
@@ -1,55 +1,32 @@
 // Run this when the extenstion icon is clicked
-
-const currentUrl = new URL(window.location.href);
-
 chrome.storage.local.get('storefronts', function (d) {
-  let currentStore = d.storefronts.filter(s => {
+  const currentUrl = new URL(window.location.href);
+  const templates = ['product', 'collection', 'page'];
+
+  const currentStore = d.storefronts.filter(s => {
     if (currentUrl.host == s.fUrl) return s;
   })[0];
 
   if (currentUrl.host == currentStore.fUrl) {
-    switch (true) {
-      case currentUrl.pathname.includes('/products'):
-        redirect(
-          currentUrl,
-          currentStore,
-          'products',
-          'var meta = {"product":{"id":',
-          ',"gid":"gid:'
-        );
-        break;
+    let templateMatched = false;
+    templates.forEach(template => {
+      if (currentUrl.pathname.includes(`/${template}s/`)) {
+        templateMatched = true;
+        redirect(currentUrl, currentStore, template);
+      }
+    });
 
-      case currentUrl.pathname.includes('/collections'):
-        redirect(
-          currentUrl,
-          currentStore,
-          'collections',
-          '"resourceType":"collection","resourceId":',
-          '}};'
-        );
-        break;
-
-      case currentUrl.pathname.includes('/pages'):
-        redirect(
-          currentUrl,
-          currentStore,
-          'pages',
-          '"resourceType":"page","resourceId":',
-          '}};'
-        );
-        break;
-
-      default:
-        window.open(`https://${currentStore.bUrl}.myshopify.com/admin`);
-    }
+    if (!templateMatched)
+      window.open(
+        `https://${currentStore.bUrl}.myshopify.com/admin/`
+      );
   }
 });
 
-async function redirect(url, store, template, startSplit, endSplit) {
-  let response = await fetch(url);
-  let rText = await response.text();
-  let id = rText.split(startSplit)[1].split(endSplit)[0];
+async function redirect(url, store, template) {
+  let response = await fetch(url + '.json');
+  let json = await response.json();
   window.open(
-    `https://${store.bUrl}.myshopify.com/admin/${template}/${id}`
+    `https://${store.bUrl}.myshopify.com/admin/${template}s/${json[template].id}`
   );
 }
